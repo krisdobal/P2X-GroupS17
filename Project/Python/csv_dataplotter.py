115cb30d3a4f81912d3e8dc8f1f14afc45bb61f4
Alexander Mizrahi-Werner <Alexander@anak.dk>
2022-06-14 10:21:55 +0200

    Update csv_dataplotter.py

--- Project/Python/csv_dataplotter.py
+++ Project/Python/csv_dataplotter.py
@@ -2,21 +2,26 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 
 
-filename = "../Dataset/Kriegers_Flak.csv"
+filename = "../Dataset/windturbine/Kriegers_Flak.csv"
 column_name = "Power"
 
 df = pd.read_csv(filename)
 column = df['Power']
 
-hours = range(0, len(column), 1)
+#hours = range(len(column)-8760, len(column), 1)
+hours = range(0, 8760, 1)
 
 
+#print(hours)
 #print(column[-1:])
 power = []
-for i in column:
-    #print(column)
-    power.append(column)
+for i in hours:
+    #print(i)
+    power.append(column[i])
     
 plt.plot(hours, power)
-#print(column[i])
 plt.show()
+
+
+cp=sum(power)/(600*8760)
+print(cp)
