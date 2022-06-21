551be1f493d8ffbce767ce01b9f649cb42624aa3
Unknown <s152955@student.dtu.dk>
2022-06-21 11:19:15 +0200

    minor changes
    Fixed scaling, removed demand, touched up the plots

--- Project/Python/technoEconomicEval_test.py
+++ Project/Python/technoEconomicEval_test.py
@@ -10,15 +10,15 @@ import plots as plots
 
 # %%
 if __name__ == "__main__":
-    nominal_power = 630  # Nominal capacity of wind farm [MW]
-    getData = ds.Dataset(nominal_power, "../Dataset/windturbine/London_Array.csv",
+    nominal_power = 605  # Nominal capacity of wind farm [MW]
+    scaleUpVal = 3*10**3
+    getData = ds.Dataset(nominal_power, "../Dataset/windturbine/Kriegers_Flak.csv",
                          "../Dataset/spot_price/elspotprices.csv", "../Dataset/demand/electricitybalancenonv.csv")
-    power = getData.getPower(2015, 1)
+    power = getData.getPower(2019, 1)
     demand = getData.getDemand()
     price_dataset = getData.getPrice()
 
-    scaled_demand, scaled_power = getData.scalePowerAndDemand(
-        demand, power, 3*10**3)
+    scaled_power = getData.scalePower(power, scaleUpVal)
     #scaled_demand, scaled_power = getData.scalePowerAndDemand_old(demand, power)
     # print(scaled_demand*.8*12)
     # print(scaled_power*12)
@@ -31,23 +31,20 @@ if __name__ == "__main__":
     time_interval = 8760
     HydrogenPrice = 8  # https://www.fchobservatory.eu/observatory/technology-and-market/levelised-cost-of-hydrogen-green-hydrogen-costs
 
-    # MinimumSpotPrice = [10, 20, 25, 30, 35, 40, 45, 50, 60, 70,80, 90, 500]#, 80, 90, 100,300,400, 500
     MinimumSpotPrice = list(range(0, 500, 30))
     LCOH_g = list(range(0, 10, 1))
 
     # Create object for hydro or elec driven class
     ElecHydro_obj = eh.ElecHydro(E_loss=E_loss, P_elec=P_elec, power_dataset=scaled_power,
-                                 demand_dataset=scaled_demand, price_dataset=price_dataset)
+                                  price_dataset=price_dataset, scaleVal = scaleUpVal)
 
-    # Plots
-    plots_obj = plots.Plots(ElecHydro_obj, time_interval, MinimumSpotPrice)
+    ### Plots ###
+    plots_obj = plots.Plots(ElecHydro_obj=ElecHydro_obj, time_interval=time_interval, MinimumSpotPrice=MinimumSpotPrice, granularity_3d=30, granularity_2d=20)
 
     # Plot of profit as a function of hydro selling price.
     #plots_obj.profit_hydroPrice(Electro_Capacity = 0, startPrice = 4, endPrice = 13, years = 3, capex = 1000, yearly_opex = 0.02, Hourly_OPEX = 1)
-
     # Plot of profit as a function of electrolyzer capacity.
-    plots_obj.profit_elecCap(SellingPrice=6, startCap=0, endCap=3 *
-                             10**3, years=5, capex=1000, yearly_opex=0.02, Hourly_OPEX=1)
+    #plots_obj.profit_elecCap(SellingPrice=6, startCap=0, endCap=12, years=5, capex=1000, yearly_opex=0.02, Hourly_OPEX=1)
 
     # Plot 3D plot of profit as a function of hydro selling price and Electrolyzer capacity.
    # plots_obj.profit_hydroPrice_elecCap(startPrice = 0, endPrice = 8,  startCap = 0, endCap = 15, years = 3, capex = 1000, yearly_opex = 0.02, Hourly_OPEX = 1)
@@ -55,10 +52,13 @@ if __name__ == "__main__":
   #  print(ElecHydro_obj.hydrogen_production(6))
 
     # 2D plot of peak shaving profit as a function of electrolyzer capacity.
-    #plots_obj.profit_PeakShaving_2d(SellingPrice = 8, startCap = 0, endCap = 16, years = 3, capex = 1000, yearly_opex = 0.02, Hourly_OPEX = 1)
+    #plots_obj.profit_PeakShaving_2d(SellingPrice = 2, startCap = 0, endCap = 3*10**3, years = 20, capex = 1000, yearly_opex = 0.02, Hourly_OPEX = 1)
 
     # 3d Plot of peak shaving profit as a function of electrolyzer capacity.
-    #plots_obj.profit_PeakShaving_3d(startPrice = 0, endPrice = 8, startCap = 0, endCap = 16, years = 5, capex = 1000, yearly_opex = 0.02, Hourly_OPEX = 1)
+    #plots_obj.profit_PeakShaving_3d(startPrice = 0, endPrice = 8, startCap = 0, endCap = 3*10**3, years = 20, capex = 1000, yearly_opex = 0.02, Hourly_OPEX = 1)
 
     # Comparison of peak shaving profit as a function of electrolyzer capacity.
-    plots_obj.profit_PeakShaving_2d_comparison(SellingPrice = 6, startCap = 0, endCap = 12, years = 5, capex = 1000, yearly_opex = 0.02, Hourly_OPEX = 1)
+    for i in range(2,10,2):
+        plots_obj.profit_PeakShaving_2d_comparison(SellingPrice = 8, startCap = 0, endCap = 3*10**3, years = 20, capex = 1000, yearly_opex = 0.02, Hourly_OPEX = 1)
+    #plots_obj.profit_PeakShaving_3d_comparison(startSellingPrice = 2, stopSellingPrice = 10, startCap = 0, endCap = 16, years = 5, capex = 1000, yearly_opex = 0.02, Hourly_OPEX = 1)
+    #############
