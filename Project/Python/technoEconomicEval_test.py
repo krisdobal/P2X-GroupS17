e8327a2492697e20d071df35656021dee493c52d
Unknown <s152955@student.dtu.dk>
2022-06-22 12:41:29 +0200

    Plots of LCOE and LCOH

--- Project/Python/technoEconomicEval_test.py
+++ Project/Python/technoEconomicEval_test.py
@@ -40,9 +40,8 @@ if __name__ == "__main__":
                                   price_dataset=price_dataset, scaleVal = scaleUpVal)
     
     ### Plots ###
-    plots_obj = plots.Plots(ElecHydro_obj=ElecHydro_obj, time_interval=time_interval, MinimumSpotPrice=MinimumSpotPrice, granularity_3d=30, granularity_2d=13)
-
-    # Plot of profit as a function of hydro selling price.
+    plots_obj = plots.Plots(ElecHydro_obj=ElecHydro_obj, time_interval=time_interval, MinimumSpotPrice=MinimumSpotPrice, granularity_3d=30, granularity_2d=10)
+     # Plot of profit as a function of hydro selling price.
     #plots_obj.profit_hydroPrice(Electro_Capacity = 0, startPrice = 4, endPrice = 13, years = 3, capex = 600, yearly_opex = 0.02, Hourly_OPEX = 1)
     # Plot of profit as a function of electrolyzer capacity.
     #plots_obj.profit_elecCap(SellingPrice=6, startCap=0, endCap=12, years=5, capex=600, yearly_opex=0.02, Hourly_OPEX=1)
@@ -56,12 +55,13 @@ if __name__ == "__main__":
     #plots_obj.profit_PeakShaving_2d(SellingPrice = 2, startCap = 0, endCap = 3*10**3, years = 20, capex = 600, yearly_opex = 0.02, Hourly_OPEX = 1)
 
     # 3d Plot of peak shaving profit as a function of electrolyzer capacity.
-    plots_obj.profit_PeakShaving_3d(startPrice = 0, endPrice = 6, startCap = 0, endCap = 3*10**3, years = 20, capex = 600, yearly_opex = 0.02, Hourly_OPEX = 1)
+    #plots_obj.profit_PeakShaving_3d(startPrice = 0, endPrice = 6, startCap = 0, endCap = 3*10**3, years = 20, capex = 600, yearly_opex = 0.02, Hourly_OPEX = 1)
 
     # Comparison of peak shaving profit as a function of electrolyzer capacity.
     sellingPrice = [1.7, 2.5, 3, 4]
     #sellingPrice = [1.7]
-    #for i in sellingPrice:
+    for i in sellingPrice:
     #    plots_obj.profit_PeakShaving_2d_comparison(SellingPrice = i, startCap = 0, endCap = 3*10**3, years = 20, capex = 600, yearly_opex = 0.02, Hourly_OPEX = 1)
-       # plt.savefig(f"elec_capVSrevenue_hydrogenPrice{i}.eps")
+        plots_obj.LCOE_and_LCOH_vs_elecCap(SellingPrice = i, startCap = 0, endCap = 3*10**3, years = 20, capex = 600, yearly_opex = 0.02, Hourly_OPEX = 1)   
+    # plt.savefig(f"elec_capVSrevenue_hydrogenPrice{i}.eps")
        #############
